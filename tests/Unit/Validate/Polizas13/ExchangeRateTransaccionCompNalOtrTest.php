ed586a8c81c3fd66c78442b4b45c584bec89ec40
Carlos C Soto <eclipxe13@gmail.com>
2021-09-10 19:34:06 -0500

    Add missing test for PLZ13COMOTREXR

---
+++ tests/Unit/Validate/Polizas13/ExchangeRateTransaccionCompNalOtrTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\CeUtils\Tests\Unit\Validate\Polizas13;
+
+use PhpCfdi\CeUtils\Tests\TestCase;
+use PhpCfdi\CeUtils\Validate\Common\BaseExchangeRate;
+use PhpCfdi\CeUtils\Validate\Polizas13\Base\ExchangeRateTransaccionCompNalOtr;
+
+final class ExchangeRateTransaccionCompNalOtrTest extends TestCase
+{
+    public function testDefinition(): void
+    {
+        $validator = ExchangeRateTransaccionCompNalOtr::create();
+        $this->assertInstanceOf(BaseExchangeRate::class, $validator);
+        $this->assertSame('PLZ13COMOTRX', $validator->getAssertCode('X'));
+        $this->assertSame(['PLZ:Poliza', 'PLZ:Transaccion', 'PLZ:CompNalOtr'], $validator->getPath());
+    }
+}
